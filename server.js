e88a128e00bb24c116bd37d5bae3194bb75bf1b8
Amos Yeo <amosyeo.sh@gmail.com>
2025-03-02 22:09:46 +0800

    done to Ex2. Ex3 not touched.

--- server.js
+++ server.js
@@ -1,89 +1,96 @@
 //git url: https://github.com/krynite/ejs-lab.git
 
-const express = require('express');
-const morgan = require("morgan")
+const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello There!');
 // });
 
-
 const RESTAURANT = {
-  name: 'The Green Byte Bistro',
+  name: "The Green Byte Bistro",
   isOpen: true,
-  address: '742 Evergreen Rd, Mapleview, OS 45502',
-  phone: '555-321-9876',
+  address: "742 Evergreen Rd, Mapleview, OS 45502",
+  phone: "555-321-9876",
   menu: [
-    { 
+    {
       id: 1,
-      name: 'Quantum Quinoa Mushroom Burger',
-      price: 13.00,
+      name: "Quantum Quinoa Mushroom Burger",
+      price: 13.0,
       rating: 4,
-      category: 'mains',
-      details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+      category: "mains",
+      details:
+        "A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.",
     },
-    { 
+    {
       id: 2,
-      name: 'Binary Berry Cheesecake',
+      name: "Binary Berry Cheesecake",
       price: 10.11,
       rating: 3,
-      category: 'desserts',
-      details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+      category: "desserts",
+      details:
+        "A creamy cheesecake bursting with flavor. A mix of berries in every byte.",
     },
-    { 
+    {
       id: 3,
-      name: 'Recursive Rigatoni',
-      price: 17.00,
+      name: "Recursive Rigatoni",
+      price: 17.0,
       rating: 5,
-      category: 'mains',
-      details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
+      category: "mains",
+      details:
+        "A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You'll keep coming back for more.",
     },
-    { 
+    {
       id: 4,
-      name: 'Pumpkin Pi Squared',
+      name: "Pumpkin Pi Squared",
       price: 3.14,
       rating: 5,
-      category: 'desserts',
-      details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+      category: "desserts",
+      details:
+        "A delightful pumpkin dessert, squared and spiced to perfection.",
     },
-    { 
+    {
       id: 5,
-      name: 'Fibonacci String Bean Fries',
+      name: "Fibonacci String Bean Fries",
       price: 11.23,
       rating: 5,
-      category: 'sides',
-      details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
-    }
-  ]
-}
+      category: "sides",
+      details:
+        "Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.",
+    },
+  ],
+};
 
 //Ex 1
 
-app.get("/", (req,res)=>{
-    // let openStatus = ''                               //! Working! 
-    // if(RESTAURANT.isOpen === true){
-    //     openStatus = "Yes we are open!"
-    // } else {
-    //     openStatus = "Sorry, we are closed."
-    // }
-    
-    let restStatus =  RESTAURANT.isOpen ? "Yes we are open!" : "Sorry, we are closed.";
+app.get("/", (req, res) => {
+  // let openStatus = ''                               //! Working!
+  // if(RESTAURANT.isOpen === true){
+  //     openStatus = "Yes we are open!"
+  // } else {
+  //     openStatus = "Sorry, we are closed."
+  // }
 
-    res.render("home.ejs", {RESTAURANT, restStatus} )
-})
+  let restStatus = RESTAURANT.isOpen
+    ? "Yes we are open!"
+    : "Sorry, we are closed.";
 
-app.get("/menu", (req,res)=>{
-    res.render("menu.ejs",)
+  res.render("home.ejs", { RESTAURANT, restStatus });
+});
 
-})
+app.get("/menu", (req, res) => {
+  res.render("menu.ejs", { menu: RESTAURANT.menu });
+});
 
+app.get("/menu/:category", (req, res) => {
+  res.render("category.ejs", { menu: RESTAURANT.menu });
+});
 
-
-app.listen(port, ()=>{
-    console.log(`Listening on Port ${port} `)
+app.listen(port, () => {
+  console.log(`Listening on Port ${port} `);
 });
